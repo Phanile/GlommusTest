99eb83cbc55c0ea4bbcccdf3b7bf5d8fa6482300
Phanile <pasha_vasilev_2003@mail.ru>
2020-07-02 20:43:01 +0300

    add models + start api

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    objects = models.Manager()
+    name = models.CharField(max_length=50, default='username')
+    money = models.IntegerField(default=0)
+    gems = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
+    damage = models.IntegerField(default=50)
+    fiends = models.ManyToManyField('Player', blank=True)
+
+    def take_all_gifts(self):
+        my_gifts = Gift.objects.get(player = self)
+        all_money = (sum([gift.money for gift in my_gifts]))
+        self.money += all_money
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class Gift(models.Model):
+    objects = models.Manager()
+    money = models.IntegerField(default=0)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Подарок'
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Player
 
-# Create your views here.
+def get_all_players(request):
+    players = Player.objects.all().values()
+    return JsonResponse(list(players), safe = False)
